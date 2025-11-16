4756fe8c05f60bbb35125e54cd058911622fb5c9
Sieun <sahn24@students.cs.ubc.ca>
2025-11-16 04:13:51 -0800

    Added details and join button in list of posts

--- src/components/DefaultPost.jsx
+++ src/components/DefaultPost.jsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
-import "./Post.css"
+import { useNavigate } from "react-router-dom";
+import "./Post.css";
+
+// import "./data/posts.json"
 
 function DefaultPost(obj) {
-    const [toggled, setToggled] =useState(false);
+    const [toggled, setToggled]=useState(false);
+    const navigate = useNavigate();
+
+    function handleJoin() {
+        navigate("/mainPage")
+    }
 
     return (
         <div className="post-bar">
             <div className="post-header">
                 <h3>{obj.restaurant}</h3>
+                <p>{obj.eat_time}</p>
+                <p>{obj.number_accepted} / {obj.max_party}</p>
+                <p>{obj.host_name}</p>
                 <button onClick={() => setToggled(!toggled)}>
-                    {toggled ? `${obj.restaurant}` : obj.comments}
+                    {toggled ? "hide" : "details"}
                 </button>
             </div>
 
             {toggled && (
                 <div className="post-details">
                     {obj.comments}
+                    <button onClick={handleJoin}>Join</button>
                 </div>
             )}
         </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
 import Post from "../components/DefaultPost.jsx";
-import { fetchPostsq } from "../api/posts.js";
 import { useNavigate } from "react-router-dom";
 
 function MainPage() {
-    const posts = [{"restaurant": "popeyes", "comments": "shoo shoo", 
-                    "restaurant": "KFC", "comments": "yum"}]
+    const posts = [{"restaurant": "Popeyes", "eat_time": "03:48:33", "max_party": 3, 
+                    "host_name": "sam", "number_accepted": 1, "comments": "shoo shoo", "username": "shahaha"}, 
+                    {"restaurant": "KFC", "eat_time": "12:50:33", "max_party": 10, 
+                    "host_name": "Billy", "number_accepted": 3, "comments": "Billy the Billy Song", "username": "bibaboo"}]
 
     const navigate = useNavigate();
 
@@ -17,8 +17,15 @@ function MainPage() {
             <h1>I'm Main Page!</h1>
             {/* list of posts */}
             <button onClick={handleCreatePost}>Host a MealMate!</button>
-            {posts.map((item, index) => (
-                <Post key={index} data={item} />
+            {posts.map((p) => (
+                <Post 
+                    key={p.restaurant} 
+                    restaurant={p.restaurant}
+                    eat_time={p.eat_time.slice(0,5)}
+                    max_party={p.max_party}
+                    number_accepted={p.number_accepted}
+                    host_name={p.host_name}
+                    comments={p.comments} />
             ))}
         </div>
     )
