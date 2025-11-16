fca2440e9f233ecc9ef59992887fd74c5dc4cb8a
Sieun <sahn24@students.cs.ubc.ca>
2025-11-15 18:23:46 -0800

    Initial Post html

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+function Post(props) {
+    return (
+        <div>
+            <h2>{props.restaurant}</h2>
+            <h3>{props.time}</h3>
+            <h3>{props.maxParty}</h3>
+            <h3>{props.host}</h3>
+        </div>
+    )
+}
+
+export default Post
