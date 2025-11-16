8c1c1df405678827861bf07fbfdf0a30b6f1893f
Sieun <sahn24@students.cs.ubc.ca>
2025-11-15 18:34:24 -0800

    Basic Skeleton for App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import { useState } from "react";
+import Post from "./components/Post"
+
+function App() {
+    return (
+        <div className="app">
+            Welcome to MealMate!
+
+            
+        </div>
+    )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import "./Post.css"
+
 function Post(props) {
     return (
         <div>
